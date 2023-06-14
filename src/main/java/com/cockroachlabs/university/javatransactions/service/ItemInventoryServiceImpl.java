baa55b41b65743c6a8b375e541fb9c57f3eedc6a
William Cross <thatnerd@gmail.com>
2023-06-14 12:31:10 -0400

    Modifying for initial state

--- src/main/java/com/cockroachlabs/university/javatransactions/service/ItemInventoryServiceImpl.java
+++ src/main/java/com/cockroachlabs/university/javatransactions/service/ItemInventoryServiceImpl.java
@@ -25,47 +25,14 @@ public class ItemInventoryServiceImpl implements ItemInventoryService {
 
     @Override
     public void updateItemInventory(UUID itemId, int quantity) throws SQLException {
+
       boolean retryTransaction = true;
 
-        /*
-        int maxRetries = 3;
-        int retryDelay = 1000;
-        int retryCount = 0;
+      // THIS IS WHERE YOU WILL MODIFY THE CODE
+      // Initially, the transaction is performed with no error handling.
+      this.updateItemInventoryTxn(itemId, quantity);
+      retryTransaction = false;
 
-  
-        while (retryTransaction) {
-            try {
-                 */
-                // to ensure a new transaction is created on every retry
-                this.updateItemInventoryTxn(itemId, quantity);
-                retryTransaction = false;
-/*
-            } catch (org.jdbi.v3.core.statement.UnableToExecuteStatementException exception) {
-
-                Throwable cause = exception.getCause();
-
-                if (cause instanceof PSQLException) {
-                    PSQLException psqlException = (PSQLException) cause;
-
-                    if ("40001".equals(psqlException.getSQLState())) {
-                        retryCount++;
-                        int delay = (int) (retryDelay * Math.pow(2, retryCount));
-                        try {
-                            Thread.sleep(delay);
-                        } catch (InterruptedException ex) {
-                            Thread.currentThread().interrupt();
-                        }
-                        if (retryCount > maxRetries) {
-                            throw new RuntimeException("Max retries exceeded", exception);
-                        }
-                        continue;
-                    }
-                }
-                throw exception;
-            }
-            
-        }
-         */
     }
 
 }
