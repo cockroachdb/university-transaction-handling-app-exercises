a7d1193400f6bc8f1b06b6dcc8e5c76ce3cc587b
kikiya <kikia@cockroachlabs.com>
2023-08-22 13:24:44 -0400

    Setup exercise 06 (#10)
    * add setup files for exercise 6

---
+++ setup/06-minimizing-contention/kwikshoppr/sql/init-minimize-breadth.sql
@@ -0,0 +1,24 @@
+-- Drop tables if they exist
+DROP IF EXISTS orders;
+DROP IF EXISTS customers;
+DROP IF EXISTS audit_logs;
+
+-- Creating the tables
+CREATE TABLE customers (
+	customer_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	name STRING,
+	email STRING UNIQUE NOT NULL
+);
+
+CREATE TABLE orders (
+	order_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	order_date TIMESTAMP,
+	customer_id UUID REFERENCES customers(customer_id)
+);
+
+CREATE TABLE audit_logs (
+	log_id UUID DEFAULT gen_random_uuid() PRIMARY KEY,
+	action STRING,
+	table_name STRING,
+	timestamp TIMESTAMP
+);
---
+++ setup/06-minimizing-contention/kwikshoppr/sql/minimize-duration.sql
@@ -0,0 +1,1 @@
+-- Create your CTE solution here
