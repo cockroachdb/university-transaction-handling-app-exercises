d2b5c6b2f3267866eb01df8244a05fc3b715b18b
Greg L. Turnquist <greg.l.turnquist@gmail.com>
2024-08-06 15:39:07 -0500

    Don't initialize inside the application.

--- exercises/code/day3-exercise07/solution/src/main/java/com/cockroachlabs/university/BooklyApplication.java
+++ exercises/code/day3-exercise07/solution/src/main/java/com/cockroachlabs/university/BooklyApplication.java
@@ -1,35 +1,12 @@
 package com.cockroachlabs.university;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.core.simple.JdbcClient;
 
 @SpringBootApplication
 public class BooklyApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(BooklyApplication.class);
-    }
-
-    @Bean
-    CommandLineRunner loadDatabase(JdbcClient jdbcClient) {
-        return args -> {
-            jdbcClient //
-                    .sql("""
-                            IMPORT INTO book (book_id, title, author, price, format, publish_date)
-                            CSV DATA ('nodelocal://self/100000_books.csv')
-                            WITH skip = '1';
-                            """) //
-                    .query() //
-                    .rowSet();
-            jdbcClient //
-                    .sql("""
-                            CREATE TABLE books_msrp AS
-                            SELECT * FROM book;
-                            """) //
-                    .update();
-        };
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(BooklyApplication.class);
+	}
 }
